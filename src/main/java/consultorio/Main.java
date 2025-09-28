1ed886b68b0ff08912f0fd0b76bfcca7369631a5
JoseDiegoSD <josediegosd13@gmail.com>
2025-09-28 13:55:12 -0700

    CalendarioPrueba

--- src/main/java/consultorio/Main.java
+++ src/main/java/consultorio/Main.java
@@ -1,12 +1,11 @@
 package consultorio;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.net.URL;
-
+//PRUEBAPUSH
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
