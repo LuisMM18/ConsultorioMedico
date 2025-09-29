f061b1d9488b16c917dea1ef7f580546cbc1ba9d
luisa <luisalbertomm18@hotmail.com>
2025-09-29 13:50:20 -0700

    botones de la vista calendario con estilos

--- src/main/java/consultorio/controlador/CalendarController.java
+++ src/main/java/consultorio/controlador/CalendarController.java
@@ -1,9 +1,18 @@
 package consultorio.controlador;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.event.ActionEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import lombok.Setter;
+import javafx.scene.control.Button;
+import java.awt.*;
+import java.io.IOException;
 
 public class CalendarController {
 
@@ -39,4 +48,36 @@ public class CalendarController {
             mainController.cambiarVistaInicio();
         }
     }
+
+    @FXML
+    public void GenerarReporte() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/GenerarReporteView.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Generar Reporte");
+            stage.initModality(Modality.APPLICATION_MODAL); // Bloquea la principal hasta cerrar
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void hoverEnter(MouseEvent e) {
+        Button btn = (Button) e.getSource();
+        btn.setStyle("-fx-background-color: #bababa;-fx-border-color: #969696; -fx-border-radius: 8px;");
+    }
+
+    @FXML
+    private void hoverExit(MouseEvent e) {
+        Button btn = (Button) e.getSource();
+        btn.setStyle("-fx-background-color: #D9D9D9; -fx-border-color: #969696; -fx-border-radius: 8px;");
+    }
+
+
+
+
 }
